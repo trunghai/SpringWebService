28477af19539dc7125db039eb3bf4f76d5658e83
HAIVAN\HAI-TPBank <trunghai9x.ptit@gmail.com>
2017-04-26 23:51:24 +0700

    ad new code

---
+++ src/main/java/com/SpringWebService/SpringWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.SpringWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebServiceApplication.class, args);
+	}
+}
